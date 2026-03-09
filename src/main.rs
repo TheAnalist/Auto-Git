fcc8117a7f229d64f11c7662c7e171e711096a29
The Analist <programmerpyth1@gmail.com>
2026-03-09 13:29:44 +0100

    Auto-Git: update main

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@
                                     Una UI di git per windows.
 
 Dev: The Analist
-
 */
+#![windows_subsystem = "windows"]
+
 mod ui;
 mod lib;
+// mod cli;
 
 use ui::gui_app;
+// use cli::cli;
 use env_logger::Env;
+
 
 #[allow(unused)]
 fn main() {
     // The `Env` lets us tweak what the environment
     // variables to read are and what the default
     // value is if they're missing
-    if cfg!(debug_assertions) {
+    if cfg!(all(windows, debug_assertions)) {
         let env = Env::default()
             .filter_or("AUTOGIT_LOG", "info")
             .write_style_or("AUTOGIT_LOG_STYLE", "always");
@@ -25,5 +29,6 @@ fn main() {
         env_logger::init_from_env(env);
     }
 
+    // cli();
     gui_app();
 }
